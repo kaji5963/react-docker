ac1a79ec2f921a0b003f0b58c311efb02096db57
kaji5963 <fd.elt.119@gmail.com>
2023-05-10 22:06:48 +0900

    ユーザー取得api作成

---
+++ React/front/src/hooks/useGetAllUsers.ts
@@ -0,0 +1,28 @@
+import { useCallback, useState } from 'react';
+import { User } from '../types/user/User';
+import { useMessage } from './useMessage';
+
+import axios from 'axios';
+
+export const useGetAllUsers = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const { showMessage } = useMessage();
+
+  const getUsers = useCallback(() => {
+    setLoading(true);
+    axios
+      .get<User[]>('http://localhost:8000/api/users/')
+      .then((res) => {
+        setUsers(res.data);
+      })
+      .catch(() => {
+        showMessage({ title: 'データ取得に失敗しました', status: 'error' });
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return { getUsers, users, loading };
+};
